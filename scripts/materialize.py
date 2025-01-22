73fbe8c1a63cde650cc0a16bc8b22464a24efde1
Jakub Peleška <jakub.peleska@fel.cvut.cz>
2025-01-22 19:06:26 +0100

    Generalize materialize script

--- scripts/materialize.py
+++ scripts/materialize.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 
-import json, sys, traceback
+import json, os, sys, traceback
 
 from multiprocessing import Process
 from pathlib import Path
@@ -12,10 +12,11 @@ from sentence_transformers import SentenceTransformer
 from torch_frame import stype
 from torch_frame.config.text_embedder import TextEmbedderConfig
 
-from relbench.base import TaskType
+from relbench.base import BaseTask, TaskType
 from relbench.datasets import get_dataset, get_dataset_names
 from relbench.tasks import get_task_names, get_task
 from relbench.modeling.graph import make_pkey_fkey_graph
+from relbench.modeling.utils import get_stype_proposal
 
 sys.path.append(".")
 
@@ -41,13 +42,19 @@ class GloveTextEmbedding:
         return self.model.encode(sentences, convert_to_tensor=True)
 
 
-def materialize_task_data(dataset_name: str, task_name):
+def materialize_task_data(dataset_name: str, task_name: Optional[str] = None):
     try:
-        cache_path = Path(f"{args["cache_dir"]}/{dataset_name}/{task_name}")
+        dataset = get_dataset(dataset_name)
 
-        dataset: DBDataset = get_dataset(dataset_name)
-        task: CTUBaseEntityTask = get_task(dataset_name, task_name)
-        db = task.get_sanitized_db(upto_test_timestamp=False)
+        if task_name is None:
+            db = dataset.get_db(upto_test_timestamp=False)
+            cache_path = Path(f"{args["cache_dir"]}/{dataset_name}")
+
+        else:
+            task: CTUBaseEntityTask = get_task(dataset_name, task_name)
+            db = task.get_sanitized_db(upto_test_timestamp=False)
+            cache_path = Path(f"{args["cache_dir"]}/{dataset_name}/{task_name}")
+
         convert_timedelta(db)
 
         stypes_cache_path = Path(f"{cache_path}/stypes.json")
@@ -59,7 +66,11 @@ def materialize_task_data(dataset_name: str, task_name):
                     if isinstance(stype_str, str):
                         col_to_stype[col] = stype(stype_str)
         except FileNotFoundError:
-            col_to_stype_dict = guess_schema(db, dataset.get_schema())
+            if isinstance(dataset, DBDataset):
+                col_to_stype_dict = guess_schema(db, dataset.get_schema())
+            else:
+                col_to_stype_dict = guess_schema(db)
+
             Path(stypes_cache_path).parent.mkdir(parents=True, exist_ok=True)
             with open(stypes_cache_path, "w") as f:
                 json.dump(col_to_stype_dict, f, indent=2, default=str)
@@ -74,26 +85,32 @@ def materialize_task_data(dataset_name: str, task_name):
                 cache_dir=f"{cache_path}/materialized",
             )
     except Exception as e:
+        Path(cache_path).mkdir(parents=True, exist_ok=True)
         with open(f"{cache_path}/error.txt", "w") as f:
             f.write(str(e))
             f.write(traceback.format_exc())
         print(f"Error: {e}")
 
 
-ctu_datasets = list(filter(lambda x: x.startswith("ctu"), get_dataset_names()))
+all_datasets = get_dataset_names()
+ctu_datasets = list(filter(lambda x: x.startswith("ctu"), all_datasets))
+relbench_datasets = list(filter(lambda x: x.startswith("rel"), all_datasets))
 
 ps: List[Process] = []
-for dataset_name in ctu_datasets:
+for dataset_name in all_datasets:
+    if dataset_name in relbench_datasets:
+        print(f"Processing {dataset_name}...")
+        p = Process(target=materialize_task_data, args=(dataset_name,))
+        ps.append(p)
+        p.start()
+        continue
+
     for task_name in get_task_names(dataset_name):
-        task: CTUBaseEntityTask = get_task(dataset_name, task_name)
-        if (
-            task.task_type
-            in [
-                TaskType.LINK_PREDICTION,
-                TaskType.MULTILABEL_CLASSIFICATION,
-            ]
-            or task.target_table is not None
-        ):
+        task = get_task(dataset_name, task_name)
+        if task.task_type in [
+            TaskType.LINK_PREDICTION,
+            TaskType.MULTILABEL_CLASSIFICATION,
+        ]:
             print(f"Skipping {dataset_name} - {task_name}...")
 
         else:
