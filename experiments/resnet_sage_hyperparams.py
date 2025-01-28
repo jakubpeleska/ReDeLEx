e6b6e18fa8767efe550e2bf2141ad33435bff591
Jakub Peleška <pelesjak@fel.cvut.cz>
2025-01-28 11:55:44 +0100

    Add Resnet SAGE experiment

---
+++ experiments/resnet_sage_hyperparams.py
@@ -0,0 +1,516 @@
+from typing import Dict, List, Optional
+
+
+import json, math, os, random, sys
+
+from argparse import ArgumentParser
+
+from datetime import datetime
+from pathlib import Path
+from timeit import default_timer as timer
+
+import ray
+from ray import tune, train as ray_train
+from ray.train.torch import enable_reproducibility
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune.logger.aim import AimLoggerCallback
+from ray.tune.logger.mlflow import MLflowLoggerCallback
+
+
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+
+import torch
+from torch.nn import BCEWithLogitsLoss, L1Loss, CrossEntropyLoss
+
+from sentence_transformers import SentenceTransformer
+from torch_frame import stype
+from torch_frame.data import StatType
+from torch_frame.config.text_embedder import TextEmbedderConfig
+
+from torch_geometric.loader import NeighborLoader
+
+
+from relbench.base import EntityTask, TaskType
+from relbench.datasets import get_dataset
+from relbench.modeling.graph import get_node_train_table_input, make_pkey_fkey_graph
+from relbench.tasks import get_task
+from relbench.metrics import (
+    accuracy,
+    average_precision,
+    f1,
+    macro_f1,
+    mae,
+    micro_f1,
+    mse,
+    r2,
+    roc_auc,
+)
+
+
+sys.path.append(".")
+
+import ctu_relational
+from ctu_relational.datasets import DBDataset
+from ctu_relational.tasks import CTUBaseEntityTask, CTUEntityTaskTemporal
+from ctu_relational.utils import (
+    guess_schema,
+    convert_timedelta,
+    standardize_db_dt,
+    standardize_table_dt,
+)
+
+from experiments.nn.sagegnn import SAGEModel
+
+
+class GloveTextEmbedding:
+    def __init__(self, device: Optional[torch.device] = None):
+        self.model = SentenceTransformer(
+            "sentence-transformers/average_word_embeddings_glove.6B.300d",
+            device=device,
+        )
+
+    def __call__(self, sentences: List[str]) -> torch.Tensor:
+        return self.model.encode(sentences, convert_to_tensor=True)
+
+
+def get_cache_path(dataset_name: str, task_name: str, cache_dir: str):
+    task = get_task(dataset_name, task_name)
+    if isinstance(task, CTUBaseEntityTask):
+        return Path(f"{cache_dir}/{dataset_name}/{task_name}")
+
+    elif isinstance(task, EntityTask):
+        return Path(f"{cache_dir}/{dataset_name}")
+
+    else:
+        raise ValueError(f"Task type {type(task)} is unsupported")
+
+
+def get_metrics(dataset_name: str, task_name: str):
+    task = get_task(dataset_name, task_name)
+
+    if task.task_type == TaskType.REGRESSION:
+        return [mae, mse, r2]
+
+    elif task.task_type == TaskType.BINARY_CLASSIFICATION:
+        return [accuracy, average_precision, f1, roc_auc]
+
+    elif task.task_type == TaskType.MULTICLASS_CLASSIFICATION:
+        return [accuracy, macro_f1, micro_f1]
+    else:
+        raise ValueError(f"Task type {task.task_type} is unsupported")
+
+
+def get_tune_metric(dataset_name: str, task_name: str):
+    task = get_task(dataset_name, task_name)
+
+    if task.task_type == TaskType.BINARY_CLASSIFICATION:
+        return "roc_auc", True
+
+    elif task.task_type == TaskType.REGRESSION:
+        return "mae", False
+
+    elif task.task_type == TaskType.MULTICLASS_CLASSIFICATION:
+        return "macro_f1", True
+    else:
+        raise ValueError(f"Task type {task.task_type} is unsupported")
+
+
+def get_loss(dataset_name: str, task_name: str):
+    task = get_task(dataset_name, task_name)
+
+    if task.task_type == TaskType.BINARY_CLASSIFICATION:
+        return BCEWithLogitsLoss(), 1
+
+    elif task.task_type == TaskType.REGRESSION:
+        return L1Loss(), 1
+
+    elif task.task_type == TaskType.MULTICLASS_CLASSIFICATION:
+        return CrossEntropyLoss(), len(task.stats[StatType.COUNT][0])
+
+    else:
+        raise ValueError(f"Task type {task.task_type} is unsupported")
+
+
+def get_data(dataset_name: str, task_name: str, cache_path: str):
+    dataset = get_dataset(dataset_name)
+    task = get_task(dataset_name, task_name)
+    if isinstance(task, CTUBaseEntityTask):
+        db = task.get_sanitized_db(upto_test_timestamp=False)
+
+    elif isinstance(task, EntityTask):
+        db = dataset.get_db(upto_test_timestamp=False)
+
+    convert_timedelta(db)
+
+    stypes_cache_path = Path(f"{cache_path}/stypes.json")
+    try:
+        with open(stypes_cache_path, "r") as f:
+            col_to_stype_dict = json.load(f)
+        for tname, col_to_stype in col_to_stype_dict.items():
+            for col, stype_str in col_to_stype.items():
+                if isinstance(stype_str, str):
+                    col_to_stype[col] = stype(stype_str)
+    except FileNotFoundError:
+        if isinstance(dataset, DBDataset):
+            col_to_stype_dict = guess_schema(db, dataset.get_schema())
+        else:
+            col_to_stype_dict = guess_schema(db)
+        Path(stypes_cache_path).parent.mkdir(parents=True, exist_ok=True)
+        with open(stypes_cache_path, "w") as f:
+            json.dump(col_to_stype_dict, f, indent=2, default=str)
+
+    standardize_db_dt(db, col_to_stype_dict)
+
+    data, col_stats_dict = make_pkey_fkey_graph(
+        db,
+        col_to_stype_dict=col_to_stype_dict,
+        text_embedder_cfg=TextEmbedderConfig(
+            text_embedder=GloveTextEmbedding(device=torch.device("cpu")), batch_size=256
+        ),
+        cache_dir=f"{cache_path}/materialized",
+    )
+
+    return task, data, col_stats_dict
+
+
+def run_experiment(config: tune.TuneConfig, cache_path: str):
+    context = ray_train.get_context()
+    experiment_dir = context.get_trial_dir()
+
+    random_seed: int = config["seed"]
+    lr: float = config["lr"]
+    min_epochs: int = config["min_epochs"]
+    batch_size: int = config["batch_size"]
+    channels: int = config["channels"]
+    num_layers: int = config["num_layers"]
+    num_neighbors: int = config["num_neighbors"]
+    max_steps_per_epoch: int = config["max_steps_per_epoch"]
+    min_total_steps: int = config["min_total_steps"]
+    aggr_fn: str = config["aggr"]
+    num_workers: int = 0
+
+    # enable_reproducibility(random_seed)
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+
+    device = torch.device("cpu")
+
+    resources = context.get_trial_resources().required_resources
+    print(f"Resources: {resources}")
+    if "GPU" in resources and resources["GPU"] > 0:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if torch.cuda.is_available():
+            torch.set_num_threads(1)
+    print("Device:", device)
+    task, data, col_stats_dict = get_data(dataset_name, task_name, cache_path)
+
+    loss_fn, out_channels = get_loss(dataset_name, task_name)
+    tune_metric, higher_is_better = get_tune_metric(dataset_name, task_name)
+    metrics = get_metrics(dataset_name, task_name)
+
+    is_temporal = isinstance(task, CTUEntityTaskTemporal) or isinstance(task, EntityTask)
+
+    loader_dict: Dict[str, NeighborLoader] = {}
+
+    for split in ["train", "val", "test"]:
+        table = task.get_table(split, mask_input_cols=False)
+        standardize_table_dt(table)
+        table_input = get_node_train_table_input(table=table, task=task)
+        loader_dict[split] = NeighborLoader(
+            data,
+            num_neighbors=[int(num_neighbors / 2**i) for i in range(num_layers)],
+            time_attr="time" if is_temporal else None,
+            input_nodes=table_input.nodes,
+            input_time=table_input.time if is_temporal else None,
+            transform=table_input.transform,
+            batch_size=batch_size,
+            temporal_strategy="uniform",
+            shuffle=split == "train",
+            num_workers=num_workers,
+            persistent_workers=num_workers > 0,
+        )
+
+    model = SAGEModel(
+        data=data,
+        col_stats_dict=col_stats_dict,
+        num_layers=num_layers,
+        channels=channels,
+        out_channels=out_channels,
+        aggr=aggr_fn,
+        norm="batch_norm",
+    ).to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr)
+
+    def train(split: str = "train") -> float:
+        model.train()
+
+        loader = loader_dict[split]
+
+        loss_accum = count_accum = 0
+        steps = 0
+        total_steps = min(len(loader), max_steps_per_epoch)
+        for batch in tqdm(loader, total=total_steps):
+            batch = batch.to(device)
+
+            optimizer.zero_grad()
+            pred = model(
+                batch,
+                task.entity_table,
+            )
+            pred = pred.view(-1) if pred.size(1) == 1 else pred
+
+            if pred.size(0) != batch[task.entity_table].batch_size:
+                pred = pred[: batch[task.entity_table].batch_size]
+
+            if task.task_type == TaskType.MULTICLASS_CLASSIFICATION:
+                target = batch[task.entity_table].y.long()
+            else:
+                target = batch[task.entity_table].y.float()
+
+            loss = loss_fn(pred.float(), target)
+            loss.backward()
+            optimizer.step()
+
+            loss_accum += loss.detach().item() * pred.size(0)
+            count_accum += pred.size(0)
+
+            steps += 1
+            if steps > max_steps_per_epoch:
+                break
+
+        return loss_accum / count_accum
+
+    @torch.no_grad()
+    def test(split: str) -> np.ndarray:
+
+        loader = loader_dict[split]
+
+        model.eval()
+
+        pred_list = []
+        for batch in tqdm(loader):
+            batch = batch.to(device)
+            pred = model(
+                batch,
+                task.entity_table,
+            )
+
+            if task.task_type in [
+                TaskType.BINARY_CLASSIFICATION,
+                TaskType.MULTILABEL_CLASSIFICATION,
+            ]:
+                pred = torch.sigmoid(pred)
+
+            if task.task_type == TaskType.MULTICLASS_CLASSIFICATION:
+                pred = torch.softmax(pred, dim=1)
+
+            pred = pred.view(-1) if pred.size(1) == 1 else pred
+
+            if pred.size(0) != batch[task.entity_table].batch_size:
+                pred = pred[: batch[task.entity_table].batch_size]
+
+            pred_list.append(pred.detach().cpu())
+        return torch.cat(pred_list, dim=0).numpy()
+
+    training_time = 0
+    best_val_metric = -math.inf if higher_is_better else math.inf
+    # report initial values
+    ray_train.report(
+        {f"val_{tune_metric}": best_val_metric, f"test_{tune_metric}": best_val_metric}
+    )
+
+    epoch_steps = min(len(loader_dict["train"]), max_steps_per_epoch)
+    n_epochs = max(math.ceil(min_total_steps / epoch_steps), min_epochs)
+
+    val_table = task.get_table("val")
+
+    model_checkpoint = os.path.join(experiment_dir, "best_model.pth")
+
+    for epoch in range(1, n_epochs + 1):
+        start = timer()
+        train_loss = train()
+        end = timer()
+
+        training_time += end - start
+
+        val_pred = test("val")
+        val_metrics = task.evaluate(val_pred, val_table, metrics=metrics)
+
+        metrics_dict = {
+            "epoch": epoch,
+            "train_loss": train_loss,
+            "train_time": training_time,
+            **{f"val_{k}": v for k, v in val_metrics.items()},
+        }
+
+        if (higher_is_better and val_metrics[tune_metric] >= best_val_metric) or (
+            not higher_is_better and val_metrics[tune_metric] <= best_val_metric
+        ):
+            best_val_metric = val_metrics[tune_metric]
+            # torch.save(model.state_dict(), model_checkpoint)
+
+            test_pred = test("test")
+            test_metrics = task.evaluate(test_pred, metrics=metrics)
+            metrics_dict.update({f"test_{k}": v for k, v in test_metrics.items()})
+
+        ray_train.report(metrics_dict)
+
+
+def run_ray_tuner(
+    dataset_name: str,
+    task_name: str,
+    ray_address: Optional[str] = None,
+    ray_storage_path: Optional[str] = None,
+    ray_experiment_name: Optional[str] = None,
+    mlflow_uri: Optional[str] = None,
+    mlflow_experiment: str = "pelesjak_resnet_sage_hyperparams",
+    aim_repo: Optional[str] = None,
+    num_samples: Optional[int] = 1,
+    random_seed: int = 42,
+    cache_dir: str = ".cache",
+):
+
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+
+    ray.init(address=ray_address, ignore_reinit_error=True, log_to_driver=False)
+
+    config = {
+        "dataset_name": dataset_name,
+        "task_name": task_name,
+        "seed": tune.randint(0, 1000),
+        # training config
+        "min_epochs": 3,
+        "max_steps_per_epoch": 1000,
+        "min_total_steps": 1000,
+        "lr": 0.001,  # tune.choice([0.001, 0.005]),
+        "batch_size": 512,  # tune.choice([128, 256, 512]),
+        # sampling config
+        "num_neighbors": tune.grid_search([16, 32, 64]),
+        # model config
+        "channels": 64,  # tune.grid_search([16, 32, 64]),
+        "num_layers": tune.grid_search([1, 2, 3, 4]),
+        "aggr": "sum",  # tune.grid_search(["max", "sum"]),
+    }
+    # scheduler = ASHAScheduler(max_t=max_num_epochs, grace_period=1, reduction_factor=2)
+    scheduler = None
+
+    if ray_experiment_name is None:
+        time = datetime.now().strftime("%Y%m%d-%H%M%S")
+        ray_experiment_name = f"resnet_sage_{time}_{dataset_name}_{task_name}"
+
+    metric, higher_is_better = get_tune_metric(dataset_name, task_name)
+    tune_metric = f"val_{metric}"
+    metric_mode = "max" if higher_is_better else "min"
+
+    cache_path = get_cache_path(dataset_name, task_name, cache_dir)
+
+    def getsize(path: str) -> int:
+        size = 0
+        for p, _, fs in os.walk(path):
+            for f in fs:
+                size += os.path.getsize(os.path.join(p, f))
+        return size
+
+    dataset_size = getsize(cache_path)
+
+    min_memory = 2e9
+    memory_limit = int(max(math.ceil((dataset_size * 2) / 1e9) * 1e9, min_memory))
+    use_gpu = min(memory_limit / 40e9, 0.5) if memory_limit > min_memory else 0
+
+    ray_callbacks = []
+    if mlflow_uri is not None:
+        ray_callbacks.append(
+            MLflowLoggerCallback(
+                tracking_uri=mlflow_uri,
+                experiment_name=mlflow_experiment,
+            )
+        )
+    if aim_repo is not None:
+        ray_callbacks.append(
+            AimLoggerCallback(repo=aim_repo, experiment_name=ray_experiment_name)
+        )
+
+    tuner = tune.Tuner(
+        tune.with_resources(
+            tune.with_parameters(run_experiment, cache_path=cache_path),
+            resources={"CPU": 1, "GPU": use_gpu, "memory": memory_limit},
+        ),
+        run_config=ray_train.RunConfig(
+            callbacks=ray_callbacks,
+            name=ray_experiment_name,
+            storage_path=ray_storage_path,
+            stop={"time_total_s": 3600 * 4},
+        ),
+        tune_config=tune.TuneConfig(
+            metric=tune_metric,
+            mode=metric_mode,
+            scheduler=scheduler,
+            num_samples=num_samples,
+            trial_name_creator=lambda trial: f"{trial.trial_id}",
+        ),
+        param_space=config,
+    )
+    results = tuner.fit()
+
+    try:
+        best_result = results.get_best_result(tune_metric, metric_mode)
+
+        print("Best trial config: {}".format(best_result.config))
+        print(f"Best trial test {metric}: {best_result.metrics[f"test_{metric}"]}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--dataset", type=str)
+    parser.add_argument("--task", type=str)
+    parser.add_argument("--ray_address", type=str, default="auto")
+    parser.add_argument("--ray_storage", type=str, default=None)
+    parser.add_argument("--run_name", type=str, default=None)
+    parser.add_argument("--mlflow_uri", type=str, default=None)
+    parser.add_argument(
+        "--mlflow_experiment", type=str, default="pelesjak_sage_hyperparam_num_layers"
+    )
+    parser.add_argument("--aim_repo", type=str, default=None)
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--num_samples", type=int, default=1)
+
+    args = parser.parse_args()
+    print(args)
+    dataset_name = args.dataset
+    task_name = args.task
+
+    task: CTUBaseEntityTask = get_task(dataset_name, task_name)
+    if task.task_type in [
+        TaskType.LINK_PREDICTION,
+        TaskType.MULTILABEL_CLASSIFICATION,
+    ]:
+        print(f"Skipping {dataset_name} - {task_name}...")
+
+    else:
+        print(f"Processing {dataset_name} - {task_name}...")
+
+        run_ray_tuner(
+            dataset_name,
+            task_name,
+            ray_address=args.ray_address,
+            ray_storage_path=(
+                os.path.realpath(args.ray_storage)
+                if args.ray_storage is not None
+                else os.path.realpath(".results")
+            ),
+            ray_experiment_name=args.run_name,
+            mlflow_uri=args.mlflow_uri,
+            mlflow_experiment=args.mlflow_experiment,
+            aim_repo=args.aim_repo,
+            random_seed=args.seed,
+            num_samples=args.num_samples,
+        )
