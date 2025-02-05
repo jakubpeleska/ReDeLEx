117f908e59af2361b4e3d7869f74de1b273d8e4b
Jakub Peleška <jakub.peleska@fel.cvut.cz>
2025-02-05 14:30:00 +0100

    Remove unused parts of SAGE gnn model

--- experiments/nn/sagegnn.py
+++ experiments/nn/sagegnn.py
@@ -28,10 +28,6 @@ class SAGEModel(torch.nn.Module):
         out_channels: int,
         aggr: str,
         norm: str,
-        # List of node types to add shallow embeddings to input
-        shallow_list: List[NodeType] = [],
-        # ID awareness
-        id_awareness: bool = False,
     ):
         super().__init__()
 
@@ -77,13 +73,7 @@ class SAGEModel(torch.nn.Module):
             norm=norm,
             num_layers=1,
         )
-        self.embedding_dict = ModuleDict(
-            {node: Embedding(data.num_nodes_dict[node], channels) for node in shallow_list}
-        )
 
-        self.id_awareness_emb = None
-        if id_awareness:
-            self.id_awareness_emb = torch.nn.Embedding(1, channels)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -91,10 +81,6 @@ class SAGEModel(torch.nn.Module):
         self.temporal_encoder.reset_parameters()
         self.gnn.reset_parameters()
         self.head.reset_parameters()
-        for embedding in self.embedding_dict.values():
-            torch.nn.init.normal_(embedding.weight, std=0.1)
-        if self.id_awareness_emb is not None:
-            self.id_awareness_emb.reset_parameters()
 
     def forward(
         self,
@@ -112,9 +98,6 @@ class SAGEModel(torch.nn.Module):
             for node_type, rel_time in rel_time_dict.items():
                 x_dict[node_type] = x_dict[node_type] + rel_time
 
-        for node_type, embedding in self.embedding_dict.items():
-            x_dict[node_type] = x_dict[node_type] + embedding(batch[node_type].n_id)
-
         x_dict = self.gnn(
             x_dict,
             batch.edge_index_dict,
@@ -127,31 +110,3 @@ class SAGEModel(torch.nn.Module):
             return self.head(x_dict[entity_table][: seed_time.size(0)])
 
         return self.head(x_dict[entity_table])
-
-    def forward_dst_readout(
-        self,
-        batch: HeteroData,
-        entity_table: NodeType,
-        dst_table: NodeType,
-    ) -> Tensor:
-        if self.id_awareness_emb is None:
-            raise RuntimeError("id_awareness must be set True to use forward_dst_readout")
-        seed_time = batch[entity_table].seed_time
-        x_dict = self.encoder(batch.tf_dict)
-        # Add ID-awareness to the root node
-        x_dict[entity_table][: seed_time.size(0)] += self.id_awareness_emb.weight
-
-        rel_time_dict = self.temporal_encoder(seed_time, batch.time_dict, batch.batch_dict)
-
-        for node_type, rel_time in rel_time_dict.items():
-            x_dict[node_type] = x_dict[node_type] + rel_time
-
-        for node_type, embedding in self.embedding_dict.items():
-            x_dict[node_type] = x_dict[node_type] + embedding(batch[node_type].n_id)
-
-        x_dict = self.gnn(
-            x_dict,
-            batch.edge_index_dict,
-        )
-
-        return self.head(x_dict[dst_table])
